eeff8e1e78f59c0d82da87ee658f662f5e601adc
MH11 <mamad.mh11@gmail.com>
2023-02-02 01:38:30 +0330

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,6 @@
-const { prefix } = require('../config.json');
+const {
+	prefix
+} = require('../config.json');
 
 module.exports = {
 	name: 'help',
@@ -8,14 +10,18 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const data = [];
-		const { commands } = message.client;
+		const {
+			commands
+		} = message.client;
 
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			return message.author.send(data, { split: true })
+			return message.author.send(data, {
+					split: true
+				})
 				.then(() => {
 					if (message.channel.type === 'dm') return;
 					message.reply('I\'ve sent you a DM with all my commands!');
@@ -41,6 +47,8 @@ module.exports = {
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
-		message.channel.send(data, { split: true });
+		message.channel.send(data, {
+			split: true
+		});
 	},
 };
