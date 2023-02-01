98acc7f048aae67233a43080052e7d87cfabe327
MH11 <mamad.mh11@gmail.com>
2023-02-02 01:37:56 +0330

    Update follow.js

--- commands/follow.js
+++ commands/follow.js
@@ -3,9 +3,9 @@ module.exports = {
 	description: 'GAMEGG Social Networks.',
 	execute(message) {
 		message.channel.send('Telegram: https://t.me/GAMEGG_IR'),
-		message.channel.send('Instagram: https://www.instagram.com/gamegg.ir'),
-		message.channel.send('Discord: https://discord.gg/2JjvhAk'),
-		message.channel.send('YouTube: https://www.youtube.com/channel/UCXjKzzdyIW-0nTJzQapeQlQ'),
-		message.channel.send('Twitch: https://www.twitch.tv/gamegg_ir');
+			message.channel.send('Instagram: https://www.instagram.com/gamegg.ir'),
+			message.channel.send('Discord: https://discord.gg/2JjvhAk'),
+			message.channel.send('YouTube: https://www.youtube.com/channel/UCXjKzzdyIW-0nTJzQapeQlQ'),
+			message.channel.send('Twitch: https://www.twitch.tv/gamegg_ir');
 	},
 };
